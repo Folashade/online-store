50499fcba0b6c0934e6a370209869478ea874be2
Folashade Okunubi <foo@andrew.cmu.edu>
2013-10-22 00:45:58 -0400

    added pg module and ports in pg.js file

---
+++ pg.js
@@ -0,0 +1,20 @@
+// var pg = require('pg');
+// 
+// pg.connect(process.env.DATABASE_URL, function(err, client) {
+//   var query = client.query('SELECT * FROM your_table');
+// 
+//   query.on('row', function(row) {
+//     console.log(JSON.stringify(row));
+//   });
+// });
+
+var pg = require('pg').native;
+
+var client = new pg.Client({
+	user: process.env.TODO_APP_DEV_USER,
+	password: process.env.TODO_APP_DEV_PASSWORD,
+	host: process.env.TODO_APP_DEV_HOST,
+	port: process.env.TODO_APP_DEV_PORT,
+	database: process.env.TODO_APP_DEV_DATABASE
+})
+
